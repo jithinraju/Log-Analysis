02ac664e2bd6a3841c573d3be053272a80235c1f
Jithin Raju <53372008+jithinraju@users.noreply.github.com>
2019-08-20 12:13:45 +0530

    simplified parser

--- logparser.py
+++ logparser.py
@@ -1,46 +1,14 @@
-#!/usr/bin/env  python3
-
-
-
 import re
 
 
-regex_host = r'(?P<host>.*?)'
-regex_identity = r'(?P<identity>\S+)'
-regex_user = r'(?P<user>\S+)'
-regex_time = r'\[(?P<time>.*?)\]'
-regex_request = r'\"(?P<request>.*?)\"'
-regex_status = r'(?P<status>\d{3})'
-regex_size = r'(?P<size>\S+)'
-regex_referer = r'\"(?P<referer>.*?)\"'
-regex_agent = r'\"(?P<agent>.*?)\"'
-regex_space = r'\s'
-
-pattern = regex_host + regex_space + regex_identity + regex_space + \
-          regex_user + regex_space + regex_time + regex_space + \
-		  regex_request + regex_space + regex_status + regex_space + \
-		  regex_size + regex_space + regex_referer + regex_space + \
-		  regex_agent
-
-
-def parser(s):
-	"""
-	return type : dict()
-	return format: {
-                       host:str , 
-                       identity:str ,
-                       user:str ,
-		       time:str ,
-                       request:str , 
-                       status:str ,
-		       size:str , 
-                       referer:str, 
-                       agent:str
-		       }
-	returns None if failed.
-	"""
-	try:
-		parts = re.match(pattern,s)
-		return parts.groupdict()
-	except Exception as err:
-		print(err)
+def parser(line):
+  
+  try:
+    
+    pattern = '(?P<host>.+?)\s(?P<identity>.+?)\s(?P<user>.+?)\s\[(?P<time>.+?)\]\s\"(?P<requests>.+?)\"\s(?P<status>\d{3})\s(?P<size>.+?)\s\"(?P<referer>.+?)\"\s\"(?P<agent>.+?)\"'
+    parseresult = re.match(pattern,line)
+    return parseresult.groupdict()
+  
+  except:
+    
+    return None
